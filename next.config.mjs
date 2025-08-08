a4919518f9ee000d160732c1dcbdef7b5b01f43e
piyushevdt <piyush.evdt@gmail.com>
2025-08-08 21:23:27 +0530

    images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'wallpaperaccess.com',], 
+    domains: [
+      "images.pexels.com",
+      "wallpaperaccess.com",
+      "static1.simpleflyingimages.com",
+      "companieslogo.com",
+      "d2n4wb9orp1vta.cloudfront.net",
+      "th.bing.com",
+      "i.redd.it",
+      "images.fineartamerica.com",
+      "logos-world.net",
+      "tse4.mm.bing.net",
+    ],
   },
 };
 
